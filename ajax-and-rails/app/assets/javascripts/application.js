4b255847747f02ab7c8e7c4156ce5ae34b128abb
ethunk <ethunk>
2018-02-04 20:30:09 -0500

    specs are passing

--- ajax-and-rails/app/assets/javascripts/application.js
+++ ajax-and-rails/app/assets/javascripts/application.js
@@ -3,3 +3,13 @@
 //= require_tree .
 
 // Add your code here.
+// app/javascripts/application.js
+
+$(function() {
+  $("form#new_comment").submit(function(event) {
+    event.preventDefault();
+    var commentForm = newCommentForm("form#new_comment");
+    var commentCreator = newCommentCreator(commentForm.attributes(), "div#comments");
+    commentCreator.create();
+  });
+});
